fb60ad2570c45e72b45a725b5f7f847cadb017e5
mounicasruthi <mounicasruthi30@gmail.com>
2022-03-13 08:30:22 +0530

    added content

--- src/components/CardsPages/Blogs.js
+++ src/components/CardsPages/Blogs.js
@@ -4,7 +4,7 @@ import { Card, ListGroup, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./CardPages.styles.css";
 
-const Blogs=()=> {
+const Blogs = () => {
   return (
     <div id="small2">
       <h1 class="heading">List of some inspiring and insightful blogs</h1>
@@ -24,14 +24,22 @@ const Blogs=()=> {
         />
         <Card.Body class="card-body">
           <div className="card-title">
-          The Importance of Networking & 5 Networking Tips
+            <h2> The Importance of Networking & 5 Networking Tips </h2>
           </div>
-          <div style={{marginTop:'10px', fontSize: '20px'}}>
+          <div style={{ marginTop: "10px", fontSize: "20px" }}>
+            There’s no denying the power a strong professional network can have
+            over your career success. Networking will help you develop and
+            improve your skill set and stay on top of the latest trends in your
+            industry and job market.
           </div>
-          <br/>
+          <br />
           <Button
             variant="secondary"
-            style={{ backgroundColor: "#9ab981",height: '2.3rem', width: '14rem' }}
+            style={{
+              backgroundColor: "#9ab981",
+              height: "2.3rem",
+              width: "14rem",
+            }}
           >
             Link to the blog
           </Button>
@@ -55,16 +63,23 @@ const Blogs=()=> {
           src="https://www.vulvani.com/wp-content/uploads/2020/02/Screenshot-2020-02-17-15.18.11.png"
         />
         <Card.Body class="card-body">
-          <div className="card-title">Period Pride: Fighting Menstruation Taboos</div>
-          <div style={{marginTop:'10px', fontSize: '20px'}}>
-          
+          <div className="card-title">
+            <h2>Period Pride: Fighting Menstruation Taboos</h2>
           </div>
-          <br/>
+          <div style={{ marginTop: "10px", fontSize: "20px" }}>
+            “Menstruation is a natural human bodily process. It is as natural as
+            breathing, sleeping or digesting food.”
+          </div>
+          <br />
           <Button
             variant="secondary"
-            style={{ backgroundColor: "#9ab981", height: '2.3rem', width: '14rem'}}
+            style={{
+              backgroundColor: "#9ab981",
+              height: "2.3rem",
+              width: "14rem",
+            }}
           >
-            Link to the website
+            Link to the blog
           </Button>
         </Card.Body>
       </Card>
@@ -87,18 +102,25 @@ const Blogs=()=> {
         />
         <Card.Body class="card-body">
           <div className="card-title">
-          All Women Deserve Respect
-
+            <h2> All Women Deserve Respect </h2>
           </div>
-          <div style={{marginTop:'10px', fontSize: '20px'}}>
+          <div style={{ marginTop: "10px", fontSize: "20px" }}>
+            We live in a modern world today but our thoughts are still stuck to
+            the age old stereotypes. There’s a serious need for us to change the
+            way we think. All women deserve respect in all cases. And there is
+            no scope of ifs and buts in this statement.
           </div>
-          <br/>
+          <br />
           <Button
             variant="secondary"
-            style={{ backgroundColor: "#9ab981", height: '2.3rem', width: '14rem'}}
+            style={{
+              backgroundColor: "#9ab981",
+              height: "2.3rem",
+              width: "14rem",
+            }}
           >
-            Link to the website
-            <br/>
+            Link to the blog
+            <br />
           </Button>
         </Card.Body>
       </Card>
@@ -106,6 +128,6 @@ const Blogs=()=> {
       <br />
     </div>
   );
-}
+};
 
 export default Blogs;
